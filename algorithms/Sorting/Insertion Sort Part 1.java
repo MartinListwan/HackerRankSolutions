8ee24d5ef28073f55836efbb32f8de276a6a726f
Martin Ciesielski-Listwan <martinc.listwan@hotmail.com>
2016-08-16 22:16:17 -0700

    beautified hackerrank solutions

--- algorithms/Sorting/Insertion Sort Part 1.java
+++ algorithms/Sorting/Insertion Sort Part 1.java
@@ -1,1 +1,41 @@
-import java.io.*; import java.util.*; import java.text.*; import java.math.*; import java.util.regex.*;  public class Solution {        public static void insertIntoSorted(int[] ar) {         // Fill up this function         if (ar.length <= 1){             printArray(ar);             return;         }          int elementToSort = ar[ar.length - 1];          int indexToReplace = 0;         for (int x = ar.length - 1; x > 0; x--){             if (elementToSort > ar[x - 1]){                 indexToReplace = x;                 break;             }             else{                 ar[x] = ar[x - 1];                 printArray(ar);             }         }         ar[indexToReplace + 1] = ar[indexToReplace];         ar[indexToReplace] = elementToSort;         printArray(ar);         // remember to remove the index of repeat and print     }  /* Tail starts here */      public static void main(String[] args) {         Scanner in = new Scanner(System.in);         int s = in.nextInt();         int[] ar = new int[s];          for(int i=0;i<s;i++){             ar[i]=in.nextInt();          }          insertIntoSorted(ar);     }       private static void printArray(int[] ar) {       for(int n: ar){          System.out.print(n+" ");       }         System.out.println("");    }   }
+import java.io.*;
+import java.util.*;
+import java.text.*;
+import java.math.*;
+import java.util.regex.*;
+public class Solution {
+ public static void insertIntoSorted(int[] ar) { // Fill up this function  
+  if (ar.length <= 1) {
+   printArray(ar);
+   return;
+  }
+  int elementToSort = ar[ar.length - 1];
+  int indexToReplace = 0;
+  for (int x = ar.length - 1; x > 0; x--) {
+   if (elementToSort > ar[x - 1]) {
+    indexToReplace = x;
+    break;
+   } else {
+    ar[x] = ar[x - 1];
+    printArray(ar);
+   }
+  }
+  ar[indexToReplace + 1] = ar[indexToReplace];
+  ar[indexToReplace] = elementToSort;
+  printArray(ar); // remember to remove the index of repeat and print     }  /* Tail starts here */  
+  public static void main(String[] args) {
+   Scanner in = new Scanner(System.in);
+   int s = in .nextInt();
+   int[] ar = new int[s];
+   for (int i = 0; i < s; i++) {
+    ar[i] = in .nextInt();
+   }
+   insertIntoSorted(ar);
+  }
+  private static void printArray(int[] ar) {
+   for (int n: ar) {
+    System.out.print(n + " ");
+   }
+   System.out.println("");
+  }
+ }
--- algorithms/implementation/save the prisoner.java
+++ algorithms/implementation/save the prisoner.java
@@ -1,1 +1,18 @@
-import java.io.*; import java.util.*;  public class Solution {      public static void main(String[] args) {         /* Enter your code here. Read input from STDIN. Print output to STDOUT. Your class should be named Solution. */         Scanner in = new Scanner(System.in);         int tests = in.nextInt();          for (int x =0; x < tests; x++){             int numOfPris = in.nextInt();             int numOfSweets = in.nextInt() - 2;             int prisID = in.nextInt();             numOfSweets = numOfSweets % numOfPris + 1;             if (numOfSweets + prisID > numOfPris){                 numOfSweets -= numOfPris;             }             System.out.println(prisID + numOfSweets);         }     } }
+import java.io.*;
+import java.util.*;
+public class Solution {
+ public static void main(String[] args) { /* Enter your code here. Read input from STDIN. Print output to STDOUT. Your class should be named Solution. */
+  Scanner in = new Scanner(System.in);
+  int tests = in .nextInt();
+  for (int x = 0; x < tests; x++) {
+   int numOfPris = in .nextInt();
+   int numOfSweets = in .nextInt() - 2;
+   int prisID = in .nextInt();
+   numOfSweets = numOfSweets % numOfPris + 1;
+   if (numOfSweets + prisID > numOfPris) {
+    numOfSweets -= numOfPris;
+   }
+   System.out.println(prisID + numOfSweets);
+  }
+ }
+}
