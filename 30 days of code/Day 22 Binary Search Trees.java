57114bf109f63020abb5ed5086d223b9ec9ae6ac
Martin Ciesielski-Listwan <martinc.listwan@hotmail.com>
2016-08-16 23:21:32 -0700

    removing folder

--- 30 days of code/Day 22 Binary Search Trees.java
+++
@@ -1,1 +0,0 @@
-	public static int getHeight(Node root){       //Write your code here         return checkHeight(root) - 1;     }  public static int checkHeight(Node root){     if (root == null){         return 0;     }     int leftHeight = checkHeight(root.left);     int rightHeight = checkHeight(root.right);     return (leftHeight > rightHeight) ? leftHeight + 1: rightHeight + 1; }
