79924db8785da094a4105bce515e59399acb6463
Martin Ciesielski-Listwan <martinc.listwan@hotmail.com>
2016-08-16 22:15:20 -0700

    Cleaned files

--- 30 days of code/Day 22 Binary Search Trees.java
+++ 30 days of code/Day 22 Binary Search Trees.java
@@ -1,1 +1,12 @@
-	public static int getHeight(Node root){       //Write your code here         return checkHeight(root) - 1;     }  public static int checkHeight(Node root){     if (root == null){         return 0;     }     int leftHeight = checkHeight(root.left);     int rightHeight = checkHeight(root.right);     return (leftHeight > rightHeight) ? leftHeight + 1: rightHeight + 1; }
+	public static int getHeight(Node root) {
+	 //Write your code here      
+	 return checkHeight(root) - 1;
+	}
+	public static int checkHeight(Node root) {
+	 if (root == null) {
+	  return 0;
+	 }
+	 int leftHeight = checkHeight(root.left);
+	 int rightHeight = checkHeight(root.right);
+	 return (leftHeight > rightHeight) ? leftHeight + 1 : rightHeight + 1;
+	}
