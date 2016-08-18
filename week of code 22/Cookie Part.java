dd6c1f206a1a1b88d1fce9832885dbd51237c783
Martin Ciesielski-Listwan <martinc.listwan@hotmail.com>
2016-08-17 23:19:23 -0700

    Beautified Week of code 22

--- week of code 22/Cookie Part.java
+++ week of code 22/Cookie Part.java
@@ -1,1 +1,25 @@
-import java.io.*; import java.util.*; import java.text.*; import java.math.*; import java.util.regex.*;  public class Solution {      public static void main(String[] args) {         Scanner in = new Scanner(System.in);         int n = in.nextInt();         int m = in.nextInt();          // more people than cookies         if (n > m){             System.out.println(n - (m % n));         } else {             m %= n;             while (m >= n){                 m -= n;             }             if (m != 0){                 System.out.println(n - (m % n));             } else {                 System.out.println(0);             }         }      } }
+import java.io.*;
+import java.util.*;
+import java.text.*;
+import java.math.*;
+import java.util.regex.*;
+public class Solution {
+ public static void main(String[] args) {
+  Scanner in = new Scanner(System.in);
+  int n = in .nextInt();
+  int m = in .nextInt(); // more people than cookies   
+  if (n > m) {
+   System.out.println(n - (m % n));
+  } else {
+   m %= n;
+   while (m >= n) {
+    m -= n;
+   }
+   if (m != 0) {
+    System.out.println(n - (m % n));
+   } else {
+    System.out.println(0);
+   }
+  }
+ }
+}
--- week of code 22/Week Of Code, Day 2.java
+++ week of code 22/Week Of Code, Day 2.java
@@ -1,1 +1,62 @@
-import java.io.*; import java.util.*; import java.text.*; import java.math.*; import java.util.regex.*;  public class Solution {      public static void main(String[] args) {         Scanner in = new Scanner(System.in);         int n = in.nextInt();         int a[] = new int[n];         for(int a_i=0; a_i < n; a_i++){             a[a_i] = in.nextInt();         }         int length = getLengthOfAllSides(a);          if (n == 3){             System.out.println(triangle(a,length));         } else {              System.out.println( total(a,length));         }     }      public static int triangle(int [] a, int length){         int totalBreaks = 0;         for (double side : a){             int currentSideBreaks = 0;             double total = (length - side);             while (side >= total){                 side /= 2;                 if (currentSideBreaks == 0){                     currentSideBreaks = 1;                 } else {                     currentSideBreaks *= 2;                 }             }             totalBreaks += currentSideBreaks;         }         return totalBreaks;     }      public static int total(int [] a, int length){         int totalBreaks = 0;         for (double side : a){             int currentSideBreaks = 0;             double total = (length) / 2.0;             while (side >= total){                 side /= 2;                 if (currentSideBreaks == 0){                     currentSideBreaks = 1;                 } else {                     currentSideBreaks *= 2;                 }             }             totalBreaks += currentSideBreaks;         }         return totalBreaks;      }      public static int getLengthOfAllSides(int [] a){         int counter  = 0;         for (int index : a){             counter+= index;         }         return counter;     } }
+import java.io.*;
+import java.util.*;
+import java.text.*;
+import java.math.*;
+import java.util.regex.*;
+public class Solution {
+ public static void main(String[] args) {
+  Scanner in = new Scanner(System.in);
+  int n = in .nextInt();
+  int a[] = new int[n];
+  for (int a_i = 0; a_i < n; a_i++) {
+   a[a_i] = in .nextInt();
+  }
+  int length = getLengthOfAllSides(a);
+  if (n == 3) {
+   System.out.println(triangle(a, length));
+  } else {
+   System.out.println(total(a, length));
+  }
+ }
+ public static int triangle(int[] a, int length) {
+  int totalBreaks = 0;
+  for (double side: a) {
+   int currentSideBreaks = 0;
+   double total = (length - side);
+   while (side >= total) {
+    side /= 2;
+    if (currentSideBreaks == 0) {
+     currentSideBreaks = 1;
+    } else {
+     currentSideBreaks *= 2;
+    }
+   }
+   totalBreaks += currentSideBreaks;
+  }
+  return totalBreaks;
+ }
+ public static int total(int[] a, int length) {
+  int totalBreaks = 0;
+  for (double side: a) {
+   int currentSideBreaks = 0;
+   double total = (length) / 2.0;
+   while (side >= total) {
+    side /= 2;
+    if (currentSideBreaks == 0) {
+     currentSideBreaks = 1;
+    } else {
+     currentSideBreaks *= 2;
+    }
+   }
+   totalBreaks += currentSideBreaks;
+  }
+  return totalBreaks;
+ }
+ public static int getLengthOfAllSides(int[] a) {
+  int counter = 0;
+  for (int index: a) {
+   counter += index;
+  }
+  return counter;
+ }
+}
